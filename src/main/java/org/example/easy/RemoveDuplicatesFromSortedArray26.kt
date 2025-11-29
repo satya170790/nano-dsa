c0c2b18cebbdaee8566bdf8e0b8cfeb0120ee497
satya170790 <smartsatya90@gmail.com>
2025-11-30 00:50:15 +0530

    S1-D2-Easy-P3

--- src/main/java/org/example/easy/RemoveDuplicatesFromSortedArray26.kt
+++ src/main/java/org/example/easy/RemoveDuplicatesFromSortedArray26.kt
@@ -1,5 +1,17 @@
 package org.example.easy
 
 class RemoveDuplicatesFromSortedArray26 {
+    fun removeDuplicates(nums: IntArray): Int {
+        val arrLen:Int = nums.size
+        if(arrLen==1) return 1
+        var xPointer =1
+        for (i in 1 until arrLen){
+            if(nums[i]!=nums[i-1]){
+                nums[xPointer]=nums[i]
+                xPointer++
+            }
+        }
 
+        return xPointer
+    }
 }

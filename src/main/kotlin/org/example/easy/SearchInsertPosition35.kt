43b4556377a3bc0c5e1ec83f6e81182b428c6129
satya170790 <smartsatya90@gmail.com>
2025-12-08 20:43:15 +0530

    S1-D9-Easy-P35

--- src/main/kotlin/org/example/easy/SearchInsertPosition35.kt
+++ src/main/kotlin/org/example/easy/SearchInsertPosition35.kt
@@ -2,7 +2,11 @@ package org.example.easy
 
 class SearchInsertPosition35 {
     fun searchInsert(nums: IntArray, target: Int): Int {
-
-        return 0
+        val len=nums.size
+        for(i in 0 until len ){
+            if(target<=nums[i])
+                return i
+        }
+        return len
     }
 }
